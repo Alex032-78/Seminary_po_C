12350f8673cce68c56fcf2d4150de55c8068f3cd
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-05 23:50:21 +0300

    Условный оператор if. Максимальное из двух чисел

--- 001/if/Program.cs
+++ 001/if/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из двух чисел.
+
+double a, b , max=0;
+string s;
+Console.Clear();
+
+Console.Write("Введите число а=");
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+
+Console.Write("Введите число b=");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a>b)
+{
+max=a;
+}
+else
+{
+max=b;
+}
+Console.WriteLine($"max={max}");
