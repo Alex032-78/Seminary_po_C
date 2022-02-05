50775c6e404b3e21185257adb0fc227a5a8b8d0c
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-06 00:05:08 +0300

    Оператор while. Сумма четных чисел от 1 до заданного

---
+++ 001/while/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму всех четных чисел от 1 до заданного значения.
+
+Console.Clear();
+int b=0,a,sum=0;
+string s;
+Console.Write("Введите целое число до которого необходимо подсчитать сумму чётных чисел а=");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+while (b<a)
+{
+    b=b+1;
+    if (b%2==0) sum=sum+b;
+}
+
+Console.WriteLine("Сумма четгых чисел {0}", sum);
