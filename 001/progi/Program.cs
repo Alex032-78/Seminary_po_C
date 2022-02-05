a229782ccfd8d0d8d69661f25ae4065fc33b9fa7
Ozimok Aleksey <jambo1ru@yandex.ru>
2022-02-05 23:20:47 +0300

    Сумма двух чисел в 001/progi

---
+++ 001/progi/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// С клавиатуры вводятся два числа. Посчитать их сумму.
+int a=1;
+int b=2;
+int sum=0;
+string s;
+Console.Clear();
+
+Console.Write("Введите число а=");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+Console.Write("Введите число b=");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+
+sum=a+b;
+
+Console.WriteLine($"{a}+{b}={sum}");
